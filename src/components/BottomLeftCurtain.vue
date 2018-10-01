b81caede1eb50bee46e16ab3bfd1516a4d66c6ab
Michael A Manzano <michael.a.manzano@intel.com>
2018-10-01 14:11:56 -0700

    getting to an initial state

---
+++ src/components/BottomLeftCurtain.vue
@@ -0,0 +1,61 @@
+<template lang="pug">
+transition(
+	leave-active-class='leave'
+	enter-active-class='enter'
+	@after-enter='onClosed'
+)
+	.blc(v-if='isVisible')
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class BottomLeftCurtain extends Vue {
+	@Prop()
+	public isVisible!: boolean;
+	@Prop()
+	public onClosed!: Function;
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../styles/colors.scss';
+.blc {
+	position: absolute;
+	bottom: 0;
+	left: 0;
+	width: 75%;
+	height: 100%;
+	background-color: orange;
+	//shape-outside: polygon(0 0, 100% 100%, 0 100%);
+	background: linear-gradient(45deg, $blueD2, $blueL1);
+	clip-path: polygon(0 0, 32% 0, 100% 100%, 0 100%);
+}
+
+.leave {
+	animation: leave 0.5s $motion;
+}
+
+@keyframes leave {
+	0% {
+		transform: translateX(0);
+	}
+	100% {
+		transform: translateX(-20rem);
+	}
+}
+
+.enter {
+	animation: enter 0.5s $motion;
+}
+
+@keyframes enter {
+	0% {
+		transform: translateX(-20rem);
+	}
+	100% {
+		transform: translateX(0);
+	}
+}
+</style>
---
+++ src/components/Links.vue
@@ -0,0 +1,28 @@
+<template lang="pug">
+.links
+	a.links__item(href='https://codepen.io/_zan0/' target='_blank') Code Pen
+	a.links__item(href='https://twitter.com/_zan0' target='_blank') Twitter
+	a.links__item(href='https://www.instagram.com/mikezan0/' target='_blank') Instagram
+	a.links__item(href='https://zan0.bandcamp.com/' target='_blank') BandCamp
+	a.links__item(href='https://soundcloud.com/djzan0' target='_blank') SoundCloud
+	a.links__item(href='' target='_blank') Facebook
+</template>
+
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Links extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+.links {
+	display: flex;
+	flex-direction: column;
+	&__item {
+		color: white;
+		font-weight: bold;
+	}
+}
+</style>
